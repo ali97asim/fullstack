3fd293fe1c6ed85fe8087a5a1f05cbdd96eb6042
ali97asim <ali.1997asim@gmail.com>
2025-03-29 16:39:30 +0500

    Added first JS script

---
+++ part1/script.js
@@ -0,0 +1,9 @@
+
+let name=prompt("Enter name:");
+let age=prompt("Enter age:");
+console.log(`Hello, ${name}! You are ${age} years old.`)
+if(parseInt(age)>=18){
+    console.log("ou are an adult");
+}else{
+    console.log("You are still a kid");
+}
