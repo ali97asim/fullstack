26a50957844d9ada38055e245f153b86167a4ae2
ali97asim <ali.1997asim@gmail.com>
2025-03-29 17:23:08 +0500

    add functions.js

---
+++ part1/functions.js
@@ -0,0 +1,18 @@
+// Task2
+//Assignment1
+function calculateFutureAge(year){
+    const d=new Date();
+    let date=prompt("Enter your birth year");
+    if(date!=""){
+    let age=2025-date;
+    console.log(`In ${2050}, you will be ${ag}`)
+    }else{
+        console.log("Invalid birthyear")
+    }
+}
+//Assignment2
+function printNumbers(n){
+    for(let i=1;i<=n;i++){
+        console.log(i+"\n");
+    }
+}
